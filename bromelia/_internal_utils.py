a68e8b5f394430ce19915009be0621a28ef4e1a0
julianeble <julianeble@yahoo.com.br>
2023-10-29 22:44:38 -0300

    Update _internal_utils.py

--- bromelia/_internal_utils.py
+++ bromelia/_internal_utils.py
@@ -174,7 +174,7 @@ def _convert_config_to_connection_obj(config) -> Connection:
         
         elif key == "TRANSPORT_TYPE":
 			if value not in ["TCP", "SCTP"]:
-                raise InvalidConfigValue("Invalid config value '{value}' "\
+				raise InvalidConfigValue("Invalid config value '{value}' "\
                                 f"found for config key '{key}'. It MUST be "\
                                 "either 'TCP' or 'SCTP'")
             transport_type = value
